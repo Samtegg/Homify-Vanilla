c623401d3aab894b25288beb31a19f99d55b7896
Samtegg <137792099+Samtegg@users.noreply.github.com>
2024-10-20 12:50:21 +0100

    Successfully connected my backend to my postgres database

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,39 @@
 import express from "express";
 import axios from "axios";
 import bodyParser from "body-parser";
-import cors from 'cors';
+import cors from "cors";
+import pg from "pg"
+
+
 
 const app = express();
 
 const port = 3000;
+
+const db = new pg.Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'homify',
+    password: '1234',
+    port: 5432
+})
+
+db.connect();
+
 
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 
+
+
 app.post('/api/register', async(req, res) => {
     const bodyParams = req.body
+
+    const resut = await db.query()
     res.json({message: 'Data received Successfully', data: bodyParams})
-    console.log(bodyParams);
+    
     
 })
 
